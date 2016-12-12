1e662ed62156d56d9683e7bb0fcbaf5c61742350
Daniel Killenberger <daniel.killenbeger@gmail.com>
2016-12-12 09:55:36 +0100

    fixed split&join at :

--- train_whatsapp_log.py
+++ train_whatsapp_log.py
@@ -1,19 +1,21 @@
 import markovify
 import json
 import utils
+import sys
 
 
 path = '_chat.txt'
 
+model_name = sys.argv[1]
 with open(path) as f:
     content = f.readlines()
 
 text = ""
 for line in content:
     temp = line.split(':')
-    temp = '_'.join(temp[:4]), '_'.join(temp[4:])
+    temp = ':'.join(temp[:4]), ':'.join(temp[4:])
     text += temp[1]
 
 model = markovify.NewlineText(text)
 
-utils.export_model(model, 'rümligruppe')
+utils.export_model(model, model_name)
